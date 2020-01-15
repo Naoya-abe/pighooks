516b39913e795d7e665d62be252c766985beba8f
阿部直也 <n.abe@gemcook.com>
2020-01-15 22:17:06 +0900

    [update] function-based App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 // import ResourceList from "./ResourceList";
 
-class App extends React.Component {
-  state = { resource: "post" };
-  render() {
-    return (
-      <div className="ui container" style={{ marginTop: "10px" }}>
-        <div>
-          <button
-            className="ui button primary"
-            onClick={() => this.setState({ resource: "post" })}
-          >
-            Posts
-          </button>
-          <button
-            className="ui button red"
-            onClick={() => this.setState({ resource: "todo" })}
-          >
-            Todos
-          </button>
-        </div>
-        {this.state.resource}
+const App = () => {
+  const [resource, setResource] = useState("post");
+  return (
+    <div className="ui container" style={{ marginTop: "10px" }}>
+      <div>
+        <button
+          className="ui button primary"
+          onClick={() => setResource("post")}
+        >
+          Posts
+        </button>
+        <button className="ui button red" onClick={() => setResource("todo")}>
+          Todos
+        </button>
       </div>
-    );
-  }
-}
+      {resource}
+    </div>
+  );
+};
 
 export default App;
