39f197ff9f20eabe586c33738d2a49793158a3bc
阿部直也 <n.abe@gemcook.com>
2020-01-15 22:13:06 +0900

    [add] class-based App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
 import React from "react";
 
+// import ResourceList from "./ResourceList";
+
 class App extends React.Component {
+  state = { resource: "post" };
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <div>
+          <button
+            className="ui button primary"
+            onClick={() => this.setState({ resource: "post" })}
+          >
+            Posts
+          </button>
+          <button
+            className="ui button red"
+            onClick={() => this.setState({ resource: "todo" })}
+          >
+            Todos
+          </button>
+        </div>
+        {this.state.resource}
+      </div>
+    );
   }
 }
 
